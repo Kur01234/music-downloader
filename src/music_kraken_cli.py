88d618a3f8e4d9a59a4f8538fdaf251b0fe348c0
Lars Noack <lars@webcontact.de>
2024-04-09 11:59:29 +0200

    feat: added development scripts

--- src/music_kraken_cli.py
+++
@@ -1,5 +0,0 @@
-import music_kraken
-
-
-if __name__ == "__main__":
-    music_kraken.cli()
--- src/tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-
-# Add the parent directory of the current file (i.e., the "tests" directory) to sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
