30829eebdbf2d307600d83b667576db5ce49cd5a
Lars Noack <lars@webcontact.de>
2024-04-10 14:09:13 +0200

    fix: added dependency versions

--- music_kraken/__init__.py
+++ music_kraken/__init__.py
@@ -9,7 +9,7 @@ from rich.console import Console
 from .utils.shared import DEBUG, DEBUG_LOGGING
 from .utils.config import logging_settings, main_settings, read_config
 
-__version__ = "1.11.0"
+__version__ = "1.12.0"
 
 read_config()
 
