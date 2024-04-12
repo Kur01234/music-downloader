7af8bacfabb28dc62a9871022d571a3924070f06
Lars Noack <lars@webcontact.de>
2024-04-12 17:23:44 +0200

    feat: added a far better example

--- development/objects_collection.py
+++ development/objects_collection.py
@@ -29,9 +29,29 @@ if __name__ == "__main__":
     a = artist.main_album_collection[0]
     b = a.song_collection[0].album_collection[0]
     c = a.song_collection[1].album_collection[0]
+    d = b.song_collection[0].album_collection[0]
+    e = d.song_collection[0].album_collection[0]
+    f = e.song_collection[0].album_collection[0]
+    g = f.song_collection[0].album_collection[0]
 
-    print(a.id, a.barcode, a.albumsort)
-    print(b.id, b.barcode, b.albumsort)
-    print(c.id, c.barcode, c.albumsort)
+    print(a.id, a.title, a.barcode, a.albumsort)
+    print(b.id, b.title, b.barcode, b.albumsort)
+    print(c.id, c.title, c.barcode, c.albumsort)
+    print(d.id, d.title, d.barcode, d.albumsort)
+    print(e.id, e.title, e.barcode, e.albumsort)
+    print(f.id, f.title, f.barcode, f.albumsort)
+    print(g.id, g.title, g.barcode, g.albumsort)
     print()
+
+    d.title = "new_title"
+
+    print(a.id, a.title, a.barcode, a.albumsort)
+    print(b.id, b.title, b.barcode, b.albumsort)
+    print(c.id, c.title, c.barcode, c.albumsort)
+    print(d.id, d.title, d.barcode, d.albumsort)
+    print(e.id, e.title, e.barcode, e.albumsort)
+    print(f.id, f.title, f.barcode, f.albumsort)
+    print(g.id, g.title, g.barcode, g.albumsort)
+    print()
+
     print(artist.main_album_collection._indexed_values)
